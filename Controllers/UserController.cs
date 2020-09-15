bdab0c36adeaa0bcb8ee767c3ca2c3b284365128
Erik Rönnberg <erik.rnnberg@gmail.com>
2020-09-15 13:22:12 +0200

    Refractoring

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,15 +5,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 using UserDataAPIApp.Models;
 
@@ -41,6 +35,7 @@ namespace UserDataAPIApp.Controllers
         public async Task<IActionResult> Register([FromBody] Register model)
         {
             var userExists = await userManager.FindByNameAsync(model.Username);
+
             if (userExists != null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
 
@@ -50,19 +45,22 @@ namespace UserDataAPIApp.Controllers
                 SecurityStamp = Guid.NewGuid().ToString(),
                 UserName = model.Username
             };
+
             var result = await userManager.CreateAsync(user, model.Password);
+
             if (!result.Succeeded)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
 
             return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
 
-        //[Authorize(Policies.Admin)]
+        [Authorize(Policies.Admin)]
         [HttpPost]
         [Route("register-admin")]
         public async Task<IActionResult> RegisterAdmin([FromBody] Register model)
         {
             var userExists = await userManager.FindByNameAsync(model.Username);
+
             if (userExists != null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
 
@@ -74,18 +72,18 @@ namespace UserDataAPIApp.Controllers
                 SocialSecurityNumber = int.Parse(model.SSN)
             };
             var result = await userManager.CreateAsync(user, model.Password);
+
             if (!result.Succeeded)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
 
             if (!await roleManager.RoleExistsAsync(Policies.Admin))
                 await roleManager.CreateAsync(new IdentityRole(Policies.Admin));
+
             if (!await roleManager.RoleExistsAsync(Policies.User))
                 await roleManager.CreateAsync(new IdentityRole(Policies.User));
 
             if (await roleManager.RoleExistsAsync(Policies.Admin))
-            {
                 await userManager.AddToRoleAsync(user, Policies.Admin);
-            }
 
             return Ok(new Response { Status = "Success", Message = "User created successfully!" });
         }
@@ -98,6 +96,7 @@ namespace UserDataAPIApp.Controllers
             var users = await userManager.Users.ToListAsync();
             if (users == null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "Failed to get users! Please check user details and try again." });
+
             var mappedResult = mapper.Map<IEnumerable<GetAll>>(users);
             return Ok(mappedResult);
         }
@@ -107,34 +106,31 @@ namespace UserDataAPIApp.Controllers
         [Route("get-user")]
         public async Task<IActionResult> GetUser([FromBody] GetUser model)
         {
-            var userExists = await userManager.FindByNameAsync(model.Username);
+            var userToGet = await userManager.FindByNameAsync(model.Username);
             var user = Request.HttpContext.User;
-            //var tokenExists = user.Identity.Name;
-            var isAdmin = user.Claims.Where(x => x.Type == "Role").Any(x => x.Value == "Admin") == true;
-            if (userExists == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound);
-            }
-            if (userExists.UserName == user.Identity.Name || isAdmin != false)
-            {
-                return Ok(new Response { Status = "Success", Message = "User found!" });
-            }
 
-            return StatusCode(StatusCodes.Status401Unauthorized);
+            if (user.Identity.Name != userToGet.UserName && user.Claims.Where(x => x.Type == "Role").Any(x => x.Value == "Admin") == false)
+                return StatusCode(StatusCodes.Status401Unauthorized, new Response { Status = "Error", Message = "Unathorized to get this user!" });
+
+            if (userToGet == null)
+                return StatusCode(StatusCodes.Status404NotFound, new Response { Status = "Error", Message = "No user was found!" });
+
+            return Ok(userToGet);
         }
 
         [Authorize]
         [HttpDelete]
         [Route("delete")]
-        public async Task<IActionResult> Delete([FromBody] Delete delete)
+        public async Task<IActionResult> Delete([FromBody] Delete model)
         {
-            var userToDelete = await userManager.FindByIdAsync(delete.id);
+            var userToDelete = await userManager.FindByIdAsync(model.id);
             var user = Request.HttpContext.User;
-            if (userToDelete == null)
-                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User does not exists!" });
 
             if (user.Identity.Name != userToDelete.UserName && user.Claims.Where(s => s.Type == "Role").Any(s => s.Value == "Admin") == false)
                 return StatusCode(StatusCodes.Status401Unauthorized, new Response { Status = "Error", Message = "Unathorized to delete this user!" });
+
+            if (userToDelete == null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User does not exists!" });
 
             var result = await userManager.DeleteAsync(userToDelete);
             if (!result.Succeeded)
